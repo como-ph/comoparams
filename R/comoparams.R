cb0622aadd2a0bdc2dc1c0406a7a6736bc77020a
Ernest Guevarra <ernest.guevarra@post.harvard.edu>
2020-04-15 09:16:30 +0100

    initial commit

---
+++ R/comoparams.R
@@ -0,0 +1,23 @@
+################################################################################
+#
+#'
+#' COVID-19 International Modelling Consortium Parameters Processor and
+#' Aggregator in R
+#'
+#' The Oxford Modelling Group for Global Health (OMGH) is developing model
+#' structures to estimate the impact of potential mitigation strategies. By
+#' changing the input data and parameter values, the models can be adjusted to
+#' represent a national or subnational setting. In addition, a user friendly
+#' application and interface is being developed to enable widespread utility.
+#' This package supports in the processing of respective national and/or
+#' subnational input data and parameter values into data structures appropriate
+#' for use with the modelling application and/or other packages developed for
+#' this purpose.
+#'
+#' @docType package
+#' @name comoparams
+#' @keywords internal
+#'
+#
+################################################################################
+"_PACKAGE"
