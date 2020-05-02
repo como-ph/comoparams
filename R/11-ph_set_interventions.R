5e7b9cfdd3e721650e84bb4549732302181bbc31
Ernest Guevarra <ernest@guevarra.io>
2020-05-02 14:21:26 +0100

    update ph_set_interventions function

--- R/11-ph_set_interventions.R
+++ R/11-ph_set_interventions.R
@@ -33,122 +33,100 @@ ph_set_intervention <- function() {
     return(cat("Thank you for using comoparams!\n"))
   }
 
-  ## Input start date of simulation
+  ## Input lockdown information
   if(set_params == 1) {
     cat("\n")
     cat("================================================================================\n")
     cat("\n")
-    cat("3. Starting date of simulation (DD/MM/YYYY format)\n")
+    lockdown <- menu(choices = c("Yes", "No"),
+                     title = "Has lockdown been implemented?")
     cat("\n")
 
-    ## Calculate comparison values
-    intStart <- lubridate::ymd("2019-12-01")
-    intEnd <- lubridate::ymd(Sys.Date()) - lubridate::weeks(4)
-    startEnd <- interval(start = intStart, end = intEnd)
+    if(lockdown == 1) {
+      repeat {
+        ## Type of lockdown
+        lockdown_level <- menu(choices = c("Low", "Mid", "High"),
+                               title = "Select level of lockdown that has been implemented?")
 
-    repeat {
-      ## Start date of simulation
-      date_range_simul_start <- readline(prompt = "Start date (DD/MM/YYYY): ")
+        ## Check of appropriate lockdown level chosen
+        if(as.numeric(lockdown_level) %in% 1:3) break
+      }
 
-      ## Check if date is in correct format
-      if(is.na(lubridate::dmy(date_range_simul_start, quiet = TRUE))) {
-        cat("\n")
-        cat("Starting date is not in DD/MM/YYYY format. Try again.\n")
-        cat("\n")
-      } else {
-        ## Check if date is within plausible starting date for simulation
-        if(!lubridate::dmy(date_range_simul_start) %within% startEnd) {
+        ## Calculate comparison values
+        intStart <- lubridate::ymd("2019-12-01")
+        intEnd <- lubridate::ymd(Sys.Date()) - lubridate::weeks(4)
+        startEnd <- interval(start = intStart, end = intEnd)
+
+      repeat {
+        ## Start date of lockdown
+        lockdown_start <- readline(prompt = "Start date (DD/MM/YYY): ")
+
+        ## Duration of lockdown
+        lockdown_duration <- readline(prompt = "Duration (weeks): ")
+
+        ## Check if date provided is within plausible range of dates
+        if(is.na(lubridate::dmy(lockdown_start, quiet = TRUE))){
           cat("\n")
-          cat("Starting date is not within plausible dates for simulation. Try again.\n")
+          cat("Lockdown starting date is not in DD/MM/YYYY format. Try again.\n")
           cat("\n")
+        } else {
+          ## Check if date is within plausible starting date for simulation
+          if(!lubridate::dmy(lockdown_start) %within% startEnd) {
+            cat("\n")
+            cat("Lockdown starting date is not within plausible dates. Try again.\n")
+            cat("\n")
+          }
+        }
+
+        ## Check if date is in correct format and plausible
+        if(!is.na(lubridate::dmy(lockdown_start, quiet = TRUE))) {
+          if(lubridate::dmy(lockdown_start) %within% startEnd) break
         }
       }
 
-      ## Check if date is in correct format and plausible
-      if(!is.na(lubridate::dmy(date_range_simul_start, quiet = TRUE))) {
-        if(lubridate::dmy(date_range_simul_start) %within% startEnd) break
+      ## Assign inputs to appropriate variables
+
+      ## Lockdown low
+      if(lockdown_level == 1) {
+        lockdown_low_switch <- TRUE
+        date_lockdown_low_on <- lockdown_start
+        lockdown_low_dur <- lockdown_duration
+      } else {
+        lockdown_low_switch <- FALSE
+        date_lockdown_low_on <- NA
+        lockdown_low_dur <- NA
+      }
+
+      ## Lockdown mid
+      if(lockdown_level == 2) {
+        lockdown_mid_switch <- TRUE
+        date_lockdown_mid_on <- lockdown_start
+        lockdown_mid_dur <- lockdown_duration
+      } else {
+        lockdown_mid_switch <- FALSE
+        date_lockdown_mid_on <- NA
+        lockdown_mid_dur <- NA
+      }
+
+      ## Lockdown high
+      if(lockdown_level == 3) {
+        lockdown_high_switch <- TRUE
+        date_lockdown_high_on <- lockdown_start
+        lockdown_high_dur <- lockdown_duration
+      } else {
+        lockdown_high_switch <- FALSE
+        date_lockdown_high_on <- NA
+        lockdown_high_dur <- NA
       }
     }
-  }
 
-  ## Input end date of simulation
-  if(!is.null(date_range_simul_start) | !is.na(date_range_simul_start)) {
-    cat("\n")
-    cat("================================================================================\n")
-    cat("\n")
-    cat("4. Ending date of simulation (DD/MM/YYYY format)\n")
-    cat("\n")
-
-    ## Calculate comparison values
-    intStart <- lubridate::ymd("2019-12-01")
-    intEnd <- lubridate::ymd(Sys.Date())
-    startEnd <- interval(start = intStart, end = intEnd)
-
-    repeat {
-      ## Start date of simulation
-      date_range_simul_end <- readline(prompt = "End date (DD/MM/YYYY): ")
-
-      ## Check if date is in correct format
-      if(is.na(lubridate::dmy(date_range_simul_end, quiet = TRUE))) {
-        cat("\n")
-        cat("Ending date is not in DD/MM/YYYY format. Try again.\n")
-        cat("\n")
-      } else {
-        ## Check if date is within plausible ending date for simulation
-        if(lubridate::dmy(date_range_simul_end) %within% startEnd) {
-          cat("\n")
-          cat("Ending date should be beyond current date. Try again.\n")
-          cat("\n")
-        }
-      }
-
-      ## Check if date is in correct format and plausible
-      if(!is.na(lubridate::dmy(date_range_simul_end, quiet = TRUE))) {
-        if(!lubridate::dmy(date_range_simul_end) %within% startEnd) break
-      }
-    }
-  }
-
-  ## Input probability of infection given contact
-  if(!is.null(date_range_simul_end) | !is.na(date_range_simul_end)) {
-    cat("\n")
-    cat("================================================================================\n")
-    cat("\n")
-    cat("5. Probability of infection given contact\n")
-    cat("\n")
-
-    repeat {
-      ## Probability of infection given contact
-      p <- readline(prompt = "Probability of infection (numeric; proportion): ")
-
-      ## Check that p is in correct format (numeric)
-      if(stringr::str_detect(string = p, pattern = "[a-zA-Z]|%")) {
-        cat("\n")
-        cat("Probability of infection (p) should be numeric. Try again.\n")
-        cat("\n")
-      } else {
-        if(as.numeric(p) <= 0) {
-          cat("\n")
-          cat("Probability of infection (p) cannot be 0 or less. Try again.\n")
-          cat("\n")
-        }
-        if(as.numeric(p) >= 1 ) {
-          cat("\n")
-          cat("Probability of infection (p) cannot be 1 or more. Try again.\n")
-          cat("\n")
-        }
-      }
-
-      ## If in correct format, go to next
-      if(stringr::str_detect(string = p, pattern = "[0-9]")) {
-        if(as.numeric(p) > 0 &
-           as.numeric(p) < 1) break
-      }
-    }
+    ##
   }
 
   ## Concatenate params
-  params <- list()
+  params <- list(lockdown_low_switch, date_lockdown_low_on, lockdown_low_dur,
+                 lockdown_mid_switch, date_lockdown_mid_on, lockdown_mid_dur,
+                 lockdown_high_switch, date_lockdown_high_on, lockdown_high_dur)
 
   ## Return params
   return(params)
