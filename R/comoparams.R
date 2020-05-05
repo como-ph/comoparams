2538003864c34efb7da7a103b2121f8263596798
Ernest Guevarra <ernest@guevarra.io>
2020-05-05 10:19:16 +0100

    update package dependencies

--- R/comoparams.R
+++ R/comoparams.R
@@ -23,7 +23,7 @@
 #' @importFrom magrittr %>%
 #' @importFrom tibble tibble
 #' @importFrom utils read.csv tail download.file menu
-#' @importFrom lubridate %within% interval ymd ymd_hms year dmy
+#' @importFrom lubridate %within% interval ymd ymd_hms year dmy days
 #' @importFrom openxlsx read.xlsx createWorkbook addWorksheet writeData
 #'   saveWorkbook getSheetNames
 #' @importFrom tidyr pivot_longer
