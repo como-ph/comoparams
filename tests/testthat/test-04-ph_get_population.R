c8a279c08ea7fb0733f36fc392f569ff6149adea
Ernest Guevarra <ernest@guevarra.io>
2020-06-24 16:39:13 +0100

    update test for population functions

--- tests/testthat/test-04-ph_get_population.R
+++ tests/testthat/test-04-ph_get_population.R
@@ -1,6 +1,23 @@
-link <- with(dataSources, link[scode == "WPP" & parameter == "population"])
-x <- ph_get_wpp2019_pop(file = link, period = 2019)
+#link <- with(dataSources, link[scode == "PSA" & parameter == "population"])
+#x <- ph_get_psa2015_pop(file = link)
+x <- population_psa_2015
 
+test_that("output is tibble", {
+  expect_is(x, "tbl")
+})
+
+test_that("output tibble has 6 columns", {
+  expect_equal(ncol(x), 6)
+})
+
+test_that("output tibble has 87567 rows", {
+  expect_equal(nrow(x), 87567)
+})
+
+#link <- with(dataSources, link[scode == "WPP" & parameter == "population"])
+#x <- ph_get_wpp2019_pop(file = link, period = 2019)
+
+x <- population_wpp_2019
 
 test_that("output is tibble", {
   expect_is(x, "tbl")
@@ -22,9 +39,10 @@ test_that("sequence of age_category is correct", {
 })
 
 
-link <- with(dataSources, link[scode == "WPP" & parameter == "births"])
-x <- ph_get_wpp2019_births(file = link, period = 2019)
+#link <- with(dataSources, link[scode == "WPP" & parameter == "births"])
+#x <- ph_get_wpp2019_births(file = link, period = 2019)
 
+x <- births_wpp_2019
 
 test_that("output is tibble", {
   expect_is(x, "tbl")
@@ -45,10 +63,10 @@ test_that("sequence of age_category is correct", {
   expect_true(x[21, "age_category"] == "100+ y.o.")
 })
 
+#link <- with(dataSources, link[scode == "WPP" & parameter == "deaths"])
+#x <- ph_get_wpp2019_deaths(file = link, period = 2019)
 
-link <- with(dataSources, link[scode == "WPP" & parameter == "deaths"])
-x <- ph_get_wpp2019_deaths(file = link, period = 2019)
-
+x <- deaths_wpp_2019
 
 test_that("output is tibble", {
   expect_is(x, "tbl")
