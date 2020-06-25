d7b2919bf50283795346e5e5dab05a5bbf575d15
Ernest Guevarra <ernest@guevarra.io>
2020-06-25 19:15:08 +0100

    use cli for interctive elements of set functions

--- R/05-ph_set_cases.R
+++ R/05-ph_set_cases.R
@@ -7,7 +7,7 @@
 #'   COVID-19 cases
 #'
 #' @examples
-#' if(interactive()) ph_set_cases()
+#' ph_set_cases()
 #'
 #' @export
 #'
@@ -18,10 +18,35 @@ ph_set_cases <- function() {
   ## Header
   cli::cli_h1(text = "Setting CoMo modelling CASES parameters for the Philippines")
   cat("\n")
-  ## Confirm if ready to proceed
-  set_params <- utils::menu(choices = c("Yes", "No"),
-                            title = "Are you ready to proceed?")
-  if(set_params == 1) {
+
+  ## Check if interactive
+  if(interactive()) {
+
+    ## Confirm if ready to proceed
+    set_params <- utils::menu(choices = c("Yes", "No"),
+                              title = "Are you ready to proceed?")
+
+    ##
+    if(set_params == 1) {
+      cat("\n")
+      cli::cli_text(text = "Calculating cases...")
+      cat("\n")
+
+      ## Calculate cases
+      cases <- ph_get_cases() %>% ph_calculate_cases()
+
+      cat("\n")
+      cli::cli_text(text = "{cli::col_green(cli::symbol$tick)} CASES parameters have been set. |
+                    {cli::col_blue(cli::symbol$arrow_right)} Proceed to next paramter.")
+
+      cat("\n")
+    } else {
+      cases <- NA
+      cat("\n")
+      cli::cli_alert_warning("CASES parameters have NOT been set.")
+      cat("\n")
+    }
+  } else {
     cat("\n")
     cli::cli_text(text = "Calculating cases...")
     cat("\n")
@@ -31,13 +56,8 @@ ph_set_cases <- function() {
 
     cat("\n")
     cli::cli_text(text = "{cli::col_green(cli::symbol$tick)} CASES parameters have been set. |
-                  {cli::col_blue(cli::symbol$arrow_right)} Proceed to next paramter.")
+                    {cli::col_blue(cli::symbol$arrow_right)} Proceed to next paramter.")
 
-    cat("\n")
-  } else {
-    cases <- NA
-    cat("\n")
-    cli::cli_alert_warning("CASES parameters have NOT been set.")
     cat("\n")
   }
 
--- R/06-ph_set_severe.R
+++ R/06-ph_set_severe.R
@@ -8,7 +8,7 @@
 #'   severity of cases and mortality
 #'
 #' @examples
-#' if(interactive()) ph_set_severe()
+#' ph_set_severe()
 #'
 #' @export
 #'
@@ -16,53 +16,45 @@
 ################################################################################
 
 ph_set_severe <- function() {
-  ## Check current date and time to see what date is the most recently
-  ## available cases data
-  #if(lubridate::now(tzone = "UTC") %within%
-  #   lubridate::interval(lubridate::ymd_hms(paste(Sys.Date(),
-  #                                                "12:00:00 UTC",
-  #                                                sep = "")),
-  #                       lubridate::ymd_hms(paste(Sys.Date(),
-  #                                                "23:59:59 UTC",
-  #                                                sep = "")))) {
-  #  refDate <- Sys.Date()
-  #} else {
-  #  refDate <- lubridate::ymd(Sys.Date()) - lubridate::days(1)
-  #}
-
   ## Header
-  cat("================================================================================\n")
+  cli::cli_h1(text = "Setting CoMo modelling SEVERITY and MORTALITY parameters for the Philippines")
   cat("\n")
-  cat("Setting CoMo modelling SEVERITY and MORTALITY parameters for the Philippines\n")
-  cat("\n")
-  cat("================================================================================\n")
-  cat("\n")
-  ## Confirm if ready to proceed
-  set_params <- utils::menu(choices = c("Yes", "No"),
-                            title = "Are you ready to proceed?")
 
-  if(set_params == 1) {
+  ## Check if interctive
+  if(interactive()) {
+    ## Confirm if ready to proceed
+    set_params <- utils::menu(choices = c("Yes", "No"),
+                              title = "Are you ready to proceed?")
+
+    if(set_params == 1) {
+      cat("\n")
+      cli::cli_text("Calculating severity and mortality...")
+
+      ##
+      sm <- ph_get_cases() %>% ph_calculate_cases()
+
+      ##
+      cat("\n")
+      cli::cli_text(text = "{cli::col_green(cli::symbol$tick)} SEVERITY and MORTALITY parameters have been set. |
+                    {cli::col_blue(cli::symbol$arrow_right)} Proceed to next parameter." )
+      cat("\n")
+    } else {
+      cases <- NA
+      cat("\n")
+      cli::cli_alert_warning(text = "SEVERITY and MORTALITY parameters have NOT been set.")
+      cat("\n")
+    }
+  } else {
     cat("\n")
-    cat("================================================================================\n")
-    cat("\n")
-    cat("Calculating severity and mortality...")
+    cli::cli_text("Calculating severity and mortality...")
 
     ##
-    #sm <- ph_calculate_rates(date = refDate)
     sm <- ph_get_cases() %>% ph_calculate_cases()
 
     ##
     cat("\n")
-    cat("================================================================================\n")
-    cat("\n")
-    cat("SEVERITY and MORTALITY parameters have been set. Proceed to next parameter.\n")
-    cat("\n")
-  } else {
-    cases <- NA
-    cat("\n")
-    cat("================================================================================\n")
-    cat("\n")
-    cat("SEVERITY and MORTALITY parameters have NOT been set.\n")
+    cli::cli_text(text = "{cli::col_green(cli::symbol$tick)} SEVERITY and MORTALITY parameters have been set. |
+                    {cli::col_blue(cli::symbol$arrow_right)} Proceed to next parameter." )
     cat("\n")
   }
 
