1d87158d784a51d37e7670f6f2db80eb23cf3543
Ernest Guevarra <ernest@guevarra.io>
2020-06-27 10:58:36 +0100

    update test for set_isolation function

--- tests/testthat/test-12-ph_set_isolation.R
+++ tests/testthat/test-12-ph_set_isolation.R
@@ -5,5 +5,7 @@ x <- ph_set_isolation()
 ##
 test_that("isolation is set", {
   expect_is(x, "list")
-  expect_true(all(is.na(x)))
+  expect_true(x[[1]] == FALSE)
+  expect_true(is.na(x[[2]]))
+  expect_true(is.na(x[[3]]))
 })
